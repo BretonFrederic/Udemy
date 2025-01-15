8ead0ea70cf893a728919e375011a8062818866e
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2025-01-16 00:50:01 +0100

    Generateur_mot_de_passe

---
+++ CSharp/Generateur_mot_de_passe/Generateur_mot_de_passe/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using FormationCS;
+
+namespace FormationCS
+{
+    class Program
+    {
+        /* FONCTIONS */
+
+        static bool DemanderChoixBinaire(string _question)
+        {
+            string choix = "";
+            Console.WriteLine(_question);
+            Console.Write("Entrer O pour oui ou N pour non : ");
+            choix = Console.ReadLine();
+            Console.WriteLine();
+            if (choix.Equals("o", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            else if (choix.Equals("n", StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("ERREUR : Choix invalide.");
+                return DemanderChoixBinaire(_question);
+            }
+        }
+        static string GenererMotPasse()
+        {
+            //Demande choisir longueur mot de passe
+            int longueurMotPasse = outils.DemanderNombrePositifNonNul("Longueur du mot de passe : ");
+
+            //Différentes chaines des choix de caractères possible
+            string alphabetMin = "abcdefghijklmnopqrstuvwxyz";
+            string alphabetMaj = alphabetMin.ToUpper();
+            string caractereNombre = "0123456789";
+            string caractereSpeciaux = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+            string alphabet = "";
+
+            //Choix des caractères du mot de passe
+            bool ajouterMin = DemanderChoixBinaire("Mot de passe avec minuscules ?");
+            if (ajouterMin)
+            {
+                alphabet += alphabetMin;
+            }
+            bool ajouterMaj = DemanderChoixBinaire("Mot de passe avec majuscules ?");
+            if (ajouterMaj)
+            {
+                alphabet += alphabetMaj;
+            }
+            bool ajouterNb = DemanderChoixBinaire("Mot de passe avec nombres ?");
+            if (ajouterNb)
+            {
+                alphabet += caractereNombre;
+            }
+            bool charSpeciaux = DemanderChoixBinaire("Mot de passe avec caractères spéciaux ?");
+            if (charSpeciaux)
+            {
+                alphabet += caractereSpeciaux;
+            }
+
+            //Génération mot de passe
+            string motDePasse = "";
+            Random aleatoire = new Random();
+            while(motDePasse.Length < longueurMotPasse)
+            {
+                for (int i = 0; i < longueurMotPasse; i++)
+                {
+                    int nbAleatoire = aleatoire.Next(alphabet.Length);
+
+                        motDePasse += alphabet[nbAleatoire];
+                }
+            }
+            return motDePasse;
+        }
+
+        /* FONCTION PRINCIPALE */
+        static void Main(string[] args)
+        {
+            bool quitter = false;
+            while (!quitter)
+            {
+
+                //Générer et afficher le mot de passe
+                string motDePasse = GenererMotPasse();
+                Console.WriteLine("Mot de passe : " + motDePasse + "\n");
+
+                //Continuer ou quitter
+                Console.WriteLine("Appuyer sur q pour quitter ou n'importe quelle touche pour continuer.");
+                string quitterStr = Console.ReadLine();
+
+                if (quitterStr.Equals("q", StringComparison.OrdinalIgnoreCase))
+                {
+                    quitter = true;
+                }
+            }
+
+        }
+    }
+}
---
+++ CSharp/Generateur_mot_de_passe/Generateur_mot_de_passe/outils.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormationCS
+{
+    static class outils
+    {
+        /* FONCTIONS */
+
+        public static int DemanderNombre(string _question)
+        {
+            int nombre = 0;
+            while (true)
+            {
+                try
+                {
+                    //Tester si valeur numérique
+                    Console.Write(_question);
+                    nombre = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                    return nombre;
+                }
+                catch
+                {
+                    Console.WriteLine("ERREUR : La valeur n'est pas un nombre.");
+                }
+            }
+        }
+        public static int DemanderNombreEntre(string _question, int _min, int _max, string _messageErreurPersonnalise)
+        {
+            int nombre = 0;
+
+            nombre = DemanderNombre(_question);
+            if (nombre >= _min && nombre <= _max)
+            {
+                return nombre;
+            }
+            else
+            {
+                if(_messageErreurPersonnalise != null)
+                {
+                    Console.WriteLine(_messageErreurPersonnalise);
+                }
+                else
+                {
+                    Console.WriteLine("Vous devez choisir un nombre entre " + _min + " et " + _max + ".");
+                }
+                return DemanderNombreEntre(_question, _min, _max, _messageErreurPersonnalise);
+            }
+        }
+
+        public static int DemanderNombrePositifNonNul(string _question)
+        {
+            return DemanderNombreEntre(_question, 1, int.MaxValue, "ERREUR : Vous devez entrer un nombre positif non nul.");
+        }
+    }
+}
