c65e59e3974b763dc57a3d1da342015a98b9a889
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2025-06-05 16:55:25 +0200

    PDO Pattern singleton et Méthode findAll pour Continents

---
+++ PHP_Website/TP_PDO_MVC_Tuto/modeles/monPdo.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *  Classe d'accès aux données. utilise les services de la classe PDO.
+ *  Les attributs sont tous statiques, les 4 premiers pour la connexion.
+ *  $monPdo qui contiendra l'unique instance de la classe.
+ */
+class MonPdo
+{
+    private static $serveur = 'mysql:host = localhost';
+    private static $bdd = 'dbname = biblio';
+    private static $user = 'root';
+    private static $mdp = '';
+    private static $monPdo;
+    private static $unPdo = null;
+
+    // Contructeur privé, crée l'instance de PDO qui sera sollicitée
+    // pour toutes les méthodes de la classe
+    private function _construct()
+    {
+        MonPdo::$unPdo = new PDO(MonPdo::$serveur.';'.MonPdo::$bdd, MonPdo::$user, MonPdo::$mdp);
+        MonPdo::$unPdo->query("SET CHARACTER SET utf8");
+        MonPdo::$unPdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    public function __destruct()
+    {
+        MonPdo::$unPdo = null;
+    }
+    /**
+     *  Fonction statique qui cree l'unique instance de la classe
+     *  Appel : $instanceMonPdo = MonPdo::getMonpdo();
+     *  @return l'unique objet de la classe MonPdo
+     */
+    public static function getinstance()
+    {
+        if(self::$unPdo == null)
+        {
+            self::$monPdo = new MonPdo();
+        }
+        return self::$unPdo;
+    }
+}
+?>
